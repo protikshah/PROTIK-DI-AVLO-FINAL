342ae6b85e732e55a4beac9ef3adfdb5254dd1f7
diabloogre573-spec <diabloogre573@gmail.com>
2026-08-14 02:25:49 +0600

    Update slot.js

--- scripts/cmds/slot.js
+++ scripts/cmds/slot.js
@@ -1,12 +1,16 @@
+const fs = require("fs-extra");
+const axios = require("axios");
+const path = require("path");
+
 module.exports = {
   config: {
     name: "slot",
     aliases: ["slots"],
-    version: "1.1",
+    version: "2.5",
     author: "Protik / Assistant",
-    countDown: 3,
+    countDown: 5,
     role: 0,
-    shortDescription: { en: "Spin the slot machine" },
+    shortDescription: { en: "Spin slot machine with video" },
     category: "games",
     guide: { en: "{pn} [bet_amount]" }
   },
@@ -15,8 +19,8 @@ module.exports = {
     const { senderID } = event;
     const bet = parseInt(args[0]);
 
-    if (isNaN(bet) || bet <= 0) return message.reply("❌ | ব্যবহারের নিয়ম: !slot [bet_amount]");
-    if (bet > 10000000) return message.reply("❌ | সর্বোচ্চ বেট লিমিট $10,000,000 (10M)!");
+    if (isNaN(bet) || bet <= 0) return message.reply("❌ | নিয়ম: !slot [bet_amount]");
+    if (bet > 50000000000) return message.reply("❌ | সর্বোচ্চ বেট লিমিট $50,000,000,000 (50 Billion)!");
 
     let userData = await usersData.get(senderID);
     let uData = userData.data || {};
@@ -30,20 +34,32 @@ module.exports = {
     const slot3 = icons[Math.floor(Math.random() * icons.length)];
 
     let winMultiplier = 0;
-    if (slot1 === slot2 && slot2 === slot3) winMultiplier = 3; 
-    else if (slot1 === slot2 || slot2 === slot3 || slot1 === slot3) winMultiplier = 1.5; 
+    if (slot1 === slot2 && slot2 === slot3) winMultiplier = 3;
+    else if (slot1 === slot2 || slot2 === slot3 || slot1 === slot3) winMultiplier = 1.5;
 
-    if (winMultiplier > 0) {
+    const isWin = winMultiplier > 0;
+    const videoUrl = isWin ? "https://i.imgur.com/K0YQ2mX.mp4" : "https://i.imgur.com/43A8gYm.mp4";
+    const cacheVideo = path.join(__dirname, "cache", `slot_${Date.now()}.mp4`);
+    await fs.ensureDir(path.join(__dirname, "cache"));
+
+    const vidRes = await axios.get(videoUrl, { responseType: "arraybuffer" });
+    await fs.writeFile(cacheVideo, Buffer.from(vidRes.data));
+
+    let msg = "";
+    if (isWin) {
       let prize = Math.floor(bet * winMultiplier);
-      let newBal = money + prize;
-      uData.money = newBal;
-      await usersData.set(senderID, { data: uData });
-      return message.reply(`🎰 SLOT MACHINE 🎰\n━━━━━━━━━━━━━━━\n[ ${slot1} | ${slot2} | ${slot3} ]\n━━━━━━━━━━━━━━━\n🎉 JACKPOT/WIN!\n💰 +$${prize.toLocaleString()}\n💵 Balance: $${newBal.toLocaleString()}`);
+      uData.money = money + prize;
+      msg = `🎰 SLOT MACHINE 🎰\n[ ${slot1} | ${slot2} | ${slot3} ]\n\n🎉 JACKPOT/WIN!\n💰 +$${prize.toLocaleString()}\n💵 Balance: $${uData.money.toLocaleString()}`;
     } else {
-      let newBal = money - bet;
-      uData.money = newBal;
-      await usersData.set(senderID, { data: uData });
-      return message.reply(`🎰 SLOT MACHINE 🎰\n━━━━━━━━━━━━━━━\n[ ${slot1} | ${slot2} | ${slot3} ]\n━━━━━━━━━━━━━━━\n😭 YOU LOST!\n💸 -$${bet.toLocaleString()}\n💵 Balance: $${newBal.toLocaleString()}`);
+      uData.money = money - bet;
+      msg = `🎰 SLOT MACHINE 🎰\n[ ${slot1} | ${slot2} | ${slot3} ]\n\n😭 YOU LOST!\n💸 -$${bet.toLocaleString()}\n💵 Balance: $${uData.money.toLocaleString()}`;
     }
+
+    await usersData.set(senderID, { data: uData });
+
+    return message.reply({
+      body: msg,
+      attachment: fs.createReadStream(cacheVideo)
+    }, () => fs.unlinkSync(cacheVideo));
   }
 };
